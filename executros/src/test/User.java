f31c22824e71721889109fb57f74046d8500bc8d
wuhan <wuhan@jsfund.com>
2021-04-29 16:20:52 +0800

    aa

---
+++ executros/src/test/User.java
@@ -0,0 +1,30 @@
+package test;
+
+/**
+ * @author os-wuhan01
+ * @title: User
+ * @projectName study
+ * @description: TODO
+ * @date 2021/4/2916:17
+ */
+public class User {
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
